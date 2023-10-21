0b5395581ceb7687e3bf6563f496055d90ef8706
Sakthivel Selvam <sakthiselvamcs@outlook.com>
2023-10-21 16:52:25 +0530

    m

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
             <div className="container nav">
                 <div className="navbar-brand pt-3 ps-3">
                     <a href="">
-                        <img src="../src/assets/s-logo.svg" alt="logo" width={40} />
+                        <img src="/s-logo.svg" alt="logo" width={40} />
                     </a>
                 </div>
             </div>
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -10,42 +10,42 @@ const Skills = () => {
                 <div className="skill-svgs d-flex flex-wrap justify-content-center align-items-center">
                     <a href="https://www.w3schools.com/html/" target='_blank' title='click to learn Html5'>
                         <div className="card skill">
-                            <img src="../../assets/html-5-svgrepo-com.svg" alt="html-svg" />
+                            <img src="/html-5-svgrepo-com.svg" alt="html-svg" />
                         </div>
                     </a>
                     <a href="https://www.w3schools.com/css/" target='_blank' title='click to learn css3'>
                         <div className="card skill">
-                            <img src="../src/assets/css-3-svgrepo-com.svg" alt="html-svg" />
+                            <img src="/css-3-svgrepo-com.svg" alt="html-svg" />
                         </div>
                     </a>
                     <a href="https://www.w3schools.com/js/" target='_blank' title='click to learn JavaScript'>
                         <div className="card skill">
-                            <img src="../src/assets/js-svgrepo-com.svg" alt="html-svg" />
+                            <img src="/js-svgrepo-com.svg" alt="html-svg" />
                         </div>
                     </a>
                     <a href="https://www.w3schools.com/typescript/" target='_blank' title='click to learn TypeScript'>
                         <div className="card skill">
-                            <img src="../src/assets/logo-ts-svgrepo-com.svg" alt="html-svg" />
+                            <img src="/logo-ts-svgrepo-com.svg" alt="html-svg" />
                         </div>
                     </a>
                     <a href="https://www.angular.io/docs/" target='_blank' title='click to learn Angular'>
                         <div className="card skill">
-                            <img src="../src/assets/angular-icon-svgrepo-com.svg" alt="html-svg" />
+                            <img src="/angular-icon-svgrepo-com.svg" alt="html-svg" />
                         </div>
                     </a>
                     <a href="https://www.getbootstrap.com/docs" target='_blank' title='click to learn Bootstrap'>
                         <div className="card skill">
-                            <img src="../src/assets/bootstrap-svgrepo-com.svg" alt="html-svg" />
+                            <img src="/bootstrap-svgrepo-com.svg" alt="html-svg" />
                         </div>
                     </a>
                     <a href="https://www.mui.com/material-ui/" target='_blank' title='click to learn Material UI'>
                         <div className="card skill">
-                            <img src="../src/assets/material-ui-svgrepo-com.svg" alt="html-svg" />
+                            <img src="/material-ui-svgrepo-com.svg" alt="html-svg" />
                         </div>
                     </a>
                     <a href="https://www.git-scm.com/doc/" target='_blank' title='click to learn Git'>
                         <div className="card skill">
-                            <img src="../src/assets/git-icon.svg" alt="html-svg" />
+                            <img src="/Git-Icon.svg" alt="html-svg" />
                         </div>
                     </a>
                 </div>
